1743f556bf12aff3d662fad371473b7a779deb75
Luka Nöckel <quack.bruchpilot@protonmail.com>
2025-12-30 14:16:13 +0100

    chore: clear-out /main

--- plugins/AG330_SASL/data/modules/Custom Module/displays/PFDcapt_popout.lua
+++
@@ -1,8 +0,0 @@
--- AeroGenesis A330-300
--- PFDcapt_popout.lua
--- by @quackderbruchpilot_ (Discord)
-
-function draw()
-	drawRectangle(0, 0, (PFDcapt_width + 3), (PFDcapt_height + 3), {0, 0, 0})
-	drawTexture(texture_PFDcapt, 0, 0, PFDcapt_width, PFDcapt_height, {1, 1, 1})
-end
